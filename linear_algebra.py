806d7cdc9819b0339563c5647b31be1fd50c98bd
Flipez <code@brauser.io>
2023-11-26 01:04:18 +0100

    dump
    Signed-off-by: Flipez <code@brauser.io>

---
+++ linear_algebra.py
@@ -0,0 +1,63 @@
+from typing import List
+
+Vector = List[float]
+
+height_weight_age = [70,  # inches
+                     170, # pounds
+                     40 ] # years
+
+grades = [95, # exam 1
+          80, # exam 2
+          75, # exam 3
+          62 ] # exam 4
+
+def add(v: Vector, w: Vector) -> Vector:
+  """Adds corresponding elements"""
+  assert len(v) == len(w), "vectors must be the same lenght"
+
+  return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+assert add([1, 2, 3], [4, 5, 6]) == [5, 7, 9]
+
+def substract(v: Vector, w: Vector) -> Vector:
+  """Substracts corresponding elements"""
+  assert len(v) == len(w), "vectors must be the same length"
+
+  return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+assert substract([5, 7, 9], [4, 5, 6]) == [1, 2, 3]
+
+def vector_sum(vectors: List[Vector]) -> Vector:
+  """Sums all corresponding elements"""
+  # check that vectors is not empty
+  assert vectors, "no vectors provided"
+
+  # check the vectors are all the same size
+  num_elements = len(vectors[0])
+  assert all(len(v) == num_elements for v in vectors), "different sizes"
+
+  # the i-th element of the result is the sum of every vector[i]
+  return [sum(vector[i] for vector in vectors)
+          for i in range(num_elements)]
+
+assert vector_sum([[1, 2], [3, 4], [5, 6], [7, 8]]) == [16, 20]
+
+def scalar_multiply(c: float, v: Vector) -> Vector:
+  """Multiplies every element by c"""
+  return [c * v_i for v_i in v]
+
+assert scalar_multiply(2, [1, 2, 3]) == [2, 4, 6]
+
+def vector_mean(vectors: List[Vector]) -> Vector:
+  """Computes the element-wise average"""
+  n = len(vectors)
+  return scalar_multiply(1/n, vector_sum(vectors))
+
+
+assert vector_mean([[1, 2], [3, 4], [5, 6]]) == [3, 4]
+
+def dot(v: Vector, w: Vector) -> float:
+  """Computes v_1 * w_1 + ... + v_n * w_n"""
+  assert len(v) == len(w), "vectors must be same length"
+  return sum(v_i * w_i for v_i, w_i in zip(v, w))
+assert dot([1, 2, 3], [4, 5, 6]) == 32 # 1 * 4 + 2 * 5 + 3 * 6
